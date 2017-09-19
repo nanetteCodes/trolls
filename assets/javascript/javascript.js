a513a60eed7c02d720dd73d8c1484d546750d399
nanette <nanettejulius@nanettes-MBP-3.attlocal.net>
2017-09-19 02:33:50 -0700

    update

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,18 +9,29 @@ $(document).ready(function() {
     for (var i = 0; i < results.length; i++) {
       // Storing the result images
       var imgURL = 'http://image.tmdb.org/t/p/w154' + results[i].poster_path;
-      var image = $("<img>").attr("src", imgURL);
+      // var image = $("<img>").attr("src", imgURL);
       var movieView = $("<div class='col m6 s12'>");
       // Storing the result titles
       var title = results[i].title;
       // Creating a h3 tag with the result item's title
-      var h3 = $("<h3>").text(title);
-      // Appending to the .movieView div
-      movieView.append(image);
-      movieView.append(h3);
+      var movieName = $("<h3>").text(title);
 
+      var movieView = $(
+            [
+              "<div class='col s12 m3 l2 main'>",
+              "<div class='card'>",
+              "<div class='card-image'>",
+              "<img src =" + imgURL + ">",
+              "</div>",
+              "<div class='card-content'>",
+              "<p class = 'center-align orange-text'>" + title + "</p>",
+              "</div>",
+              "</div>",
+              "</div>"
+            ].join("")
+          );
       // Prepending the movieView to the HTML
-      $("#moviesDiv").prepend(movieView);
+      $("#moviesDiv").append(movieView);
 
     }
   });
